96bba01283b96ddbcb777094a1546c22486b8e27
Shubham Kale <kaleshubham16@gmail.com>
2025-01-21 11:56:25 +0530

    2st Test - single string

--- String_Calculator.test.js
+++ String_Calculator.test.js
@@ -3,3 +3,7 @@ const add = require('./String_Calculator');
 test('returns 0 for an empty string', () => {
     expect(add('')).toBe(0);
 });
+
+test('returns the number single digit', () => {
+    expect(add('1')).toBe(1);
+});
