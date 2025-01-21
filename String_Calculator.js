a6ecfcdc3bb024bde6c3af64dddf1d4db1d5f326
Shubham Kale <kaleshubham16@gmail.com>
2025-01-21 11:59:54 +0530

    3st Test - 2 string

--- String_Calculator.js
+++ String_Calculator.js
@@ -1,7 +1,12 @@
 function add(numbers){
     if(numbers === '') return 0;
 
-    return parseInt(numbers);
+    // return parseInt(numbers); //For Test 2
+
+    // As numbers is a string , split it by comma and convert it to array of nums
+    const nums = numbers.split(',').map(num => parseInt(num));
+    return nums.reduce((sum, num) => sum + num); // calculate the sum
+
 }
 
 module.exports = add;
--- String_Calculator.test.js
+++ String_Calculator.test.js
@@ -4,6 +4,10 @@ test('returns 0 for an empty string', () => {
     expect(add('')).toBe(0);
 });
 
-test('returns the number single digit', () => {
+test('returns the number single number', () => {
     expect(add('1')).toBe(1);
+});
+
+test('returns sum of two numbers', () => {
+    expect(add('10,20')).toBe(30);
 });
